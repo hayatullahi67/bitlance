7c8bfb80199809620970ad1410698704e31184fe
unknown <wahadisllahi@gmail.com>
2025-07-17 11:17:55 +0100

    sat

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ReactGA 'react-ga4'
+import ReactGA from 'react-ga4'
 
 ReactGA.initialize("G-8GVSSPRZXM");
-ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Custom Title" });
+ReactGA.send({ hitType: "pageview", page: window.location.pathname, });
 
 createRoot(document.getElementById("root")!).render(<App />);
